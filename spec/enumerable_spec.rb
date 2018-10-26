fa74b5b0d2794c7d5641b779c3ae6bfbce87c779
Steven G. Harms <steven.harms@flatironschool.com>
2018-10-26 16:08:54 -0400

    Scourge whitespace

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 enumerable_array_1 = ['pepperoni', 'sausage', 'green olives', 'green peppers', 'onions', 'pineapple', 'anchovies']
 enumerable_array_2 = ['This', 'is', 'true', 'love', 'you', 'think', 'this', 'happens', 'every', 'day']
 enumerable_array_3 = ['I', 3, 'have', 97, 'strings', 3.14159, 'floats', 'and', 8675309, 'integers', 42, 'in', 'the', 'same', 'enumerable']
@@ -8,32 +9,32 @@ enumerable_array_5 = [86262, 58115, 22558, 97709, 66236, 28056, 25992, 81375, 11
 
 # Question 1
 # Write a method that uses map or collect
-describe "#using_map" do 
-  it "takes in argument of an array and returns a new array of statments with each topping mapped to each statement" do 
+describe "#using_map" do
+  it "takes in argument of an array and returns a new array of statments with each topping mapped to each statement" do
     expect(using_map(enumerable_array_1)).to eq(["I love pepperoni on my pizza!", "I love sausage on my pizza!", "I love green olives on my pizza!", "I love green peppers on my pizza!", "I love onions on my pizza!", "I love pineapple on my pizza!", "I love anchovies on my pizza!"] )
   end
 end
 
 # Question 2
 # Write a method that uses select
-describe "#using_select" do 
-  it "takes in argument of an array and uses select to return the strings" do 
+describe "#using_select" do
+  it "takes in argument of an array and uses select to return the strings" do
     expect(using_select(enumerable_array_3)).to eq(["I", "have", "strings", "floats", "and", "integers", "in", "the", "same", "enumerable"])
   end
 end
 
 # Question 3
 # Write a method that uses find
-describe "#using_find" do 
-  it "takes in argument of an array and uses find to return the first number divisible by 5 and 15" do 
+describe "#using_find" do
+  it "takes in argument of an array and uses find to return the first number divisible by 5 and 15" do
     expect(using_find(enumerable_array_4)).to eq(15)
   end
 end
 
 # Question 4
 # Write a method that uses each
-describe "#using_each" do 
-  it "takes in argument of an array and uses each to output a statement with an array interpolated within it" do 
+describe "#using_each" do
+  it "takes in argument of an array and uses each to output a statement with an array interpolated within it" do
     expect(using_each(enumerable_array_2)).to eq(["This", "is", "true", "love", "you", "think", "this", "happens", "every", "day"])
   end
 end
@@ -41,8 +42,8 @@ end
 
 # Question 5
 # Write a method that uses sort
-describe "#using_sort" do 
-  it "takes in an argument of an array and returns the sorted array" do 
+describe "#using_sort" do
+  it "takes in an argument of an array and returns the sorted array" do
     expect(using_sort(enumerable_array_2).last).to eq("you")
     expect(using_sort(enumerable_array_2).first).to eq("This")
   end
@@ -50,15 +51,15 @@ end
 
 # Question 6
 # Write a method that uses max
-describe "#using_max" do 
-  it "takes in argument of an array and returns the highest numerical value" do 
+describe "#using_max" do
+  it "takes in argument of an array and returns the highest numerical value" do
     expect(using_max(enumerable_array_5)).to eq(8675309)
   end
 end
 
 # Question 7
 # Write a method that uses include?
-describe "#using_include" do 
+describe "#using_include" do
   it "takes in arguments of an array and an element and returns true if the array includes that element, false if it does not." do
     element1 = "This"
     element2 = "Tardis"
@@ -69,16 +70,16 @@ end
 
 # Question 8
 # Write a method that uses all?
-describe "#using_all?" do 
-  it "takes in argument of an array and returns true if all of the numbers in the array are even" do 
+describe "#using_all?" do
+  it "takes in argument of an array and returns true if all of the numbers in the array are even" do
     expect(using_all?(enumerable_array_5)).to eq(false)
   end
 end
 
 # Question 9
 # Write a method that uses any?
-describe "#using_any?" do 
-  it "takes in argument of an array and returns true if there are any integers in the given array" do 
+describe "#using_any?" do
+  it "takes in argument of an array and returns true if there are any integers in the given array" do
     expect(using_any?(enumerable_array_2)).to eq(false)
   end
 end
